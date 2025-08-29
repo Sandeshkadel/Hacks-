872b972ef9e32a9c61d6f2b6b8d1687ea9855371
Sandesh kandel <sandeshkandel2474@gmail.com>
2025-08-29 17:06:44 +0545

    Add Firebase configuration values to config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,11 +1,20 @@
-// Fill with your Firebase project settings to enable permanent storage and admin auth.
+// Replace the empty strings with your actual Firebase Web App config values
 window.AppConfig = {
   firebase: {
-    apiKey: "",               // required
-    authDomain: "",           // required
-    projectId: "",            // required
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyD5Mfvjir8lhmRoRZxWjXIv54JXiYYyL0A",
+    authDomain: "techcrafters-2919c.firebaseapp.com",
+    projectId: "techcrafters-2919c",
+    storageBucket: "techcrafters-2919c.firebasestorage.app",
+    messagingSenderId: "1039652089214",
+    appId: "1:1039652089214:web:dc55c2fd07fee95c5e972f"
+     measurementId: "G-RSMJYSXHSQ"
+  },
+  // optional: EmailJS if you want real emails instead of mock outbox
+  email: {
+    emailjs: {
+      serviceId: "",    // e.g. "service_abc123"
+      templateId: "",   // e.g. "template_xyz789"
+      publicKey: ""     // e.g. "KLMNO_PQR"
+    }
   }
 };
